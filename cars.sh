bcc1dd74b8da16837df7f01d0aa8f1b6c0f47cfc
brian byrd <69371288+BByrdR@users.noreply.github.com>
2020-12-08 22:38:16 -0800

    Create cars.sh

---
+++ cars.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#cars.sh
+#Brian Byrd
+i ="number typed"
+while [ i = 0; i !=3;]
+do
+  case $i in
+echo type the number 1 to enter a new car.
+echo "what is the year,make,model."
+echo type the number 2 to display the list of cars.
+  read My_old_cars
+echo type the number 3 to quit and exit the program.
+echo GoodBye
+  esac  
+done
